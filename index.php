fbe0e410fd12a10bdf5ae0112d906dae2b1c51fa
Camila da Costa <camila.coosta99@gmail.com>
2023-01-18 23:39:41 -0300

    FEAT(aula02): adicionando botão individual para remoção e validando campo vazio

--- index.php
+++ index.php
@@ -7,18 +7,28 @@ if(!isset($_SESSION['tasks'])){ //VERIFICA SE NA SESSÃO INICIADA A CHAVE ['task
 }
 
 if(isset($_GET['task_name'])){ //VERIFICA SE EXISTE task_name NA REQUISIÇÃO
-    array_push($_SESSION['tasks'], $_GET['task_name']); // SE EXISTIR ADICIONA O VALOR DENTRO DA SESSÃO['tasks'] INICIADA ACIMA
-    unset($_GET['task_name']); //REMOVE O PARAMETRO DA REQUISICAO
+    if($_GET['task_name'] != ""){
+        array_push($_SESSION['tasks'], $_GET['task_name']); // SE EXISTIR ADICIONA O VALOR DENTRO DA SESSÃO['tasks'] INICIADA ACIMA
+        unset($_GET['task_name']); //REMOVE O PARAMETRO DA REQUISICAO
+    }else{
+        $_SESSION['message'] = "O campo nome da tarefa não pode ser vazio";
+    }
 }
 
 if(isset($_GET['clear'])){ //VERIFICA SE EXISTE clear NA REQUISIÇÃO
-    unset($_SESSION['tasks']); //REMOVE OS VALORES PASSADOS NA SESSION[]
+    unset($_SESSION['tasks']); //REMOVE OS VALORES PASSADOS NA SESSION['tasks']
+    unset($_GET['clear']); //REMOVE OS VALORES PASSADOS NA SESSION['clear']
 }
 #
 #em resumo inicia a variavel global $_SESSION e cria nela um array vazio 
 #com a chave de referencia ['tasks'] e atraves do get submetido no form
 #pega os valores inseridos pelo usuário e armazena na sessão['tasks']
 #
+
+if(isset($_GET['key'])){ //VERIFICA SE EXISTE key NA REQUISIÇÃO | Key OBTIDO PELA FUNÇÃO JS DO FORM
+    array_splice($_SESSION['tasks'], $_GET['key'], 1); //FUNÇÃO QUE REMOVE ELEMENTOS DO ARRAY ['tasks] na posição ['key']
+    unset($_GET['key']); //REMOVE OS VALORES PASSADOS NA SESSION['key']
+}
 ?>
 
 <!DOCTYPE html>
@@ -42,6 +52,12 @@ if(isset($_GET['clear'])){ //VERIFICA SE EXISTE clear NA REQUISIÇÃO
                 <input type="text" name="task_name" placeholder="Nome da Tarefa">
                 <button type="submit">Cadastrar</button>
             </form>
+            <?php
+                if(isset($_SESSION['message'])){
+                    echo "<p style='color:#3615c8;'>" . $_SESSION['message'] . "</p>";
+                    unset($_SESSION['message']);
+                }
+            ?>
         </div>
         <div class="separator">
         </div>
@@ -50,7 +66,19 @@ if(isset($_GET['clear'])){ //VERIFICA SE EXISTE clear NA REQUISIÇÃO
                 if(isset($_SESSION['tasks'])){
                     echo "<ul>";
                     foreach ($_SESSION['tasks'] as $key => $task){
-                        echo "<li>$task</li>";
+                        echo "<li>
+                                <span>$task</span>
+                                <button type='button' class='btn-clear' onclick='deletar$key()'>Remover</button>
+                                <script>
+                                    function deletar$key(){
+                                        if(confirm('Confirmar remoção?')){
+                                            window.location = 'http://localhost:88/?key=$key';
+                                        }
+                                        return false;
+                                    }
+                                </script>
+                              </li>";
+                        #deletar$key => redireciona para a mesma url passando o valor de $key especificado pela posição para fazer o remove
                     }
                     echo "</ul>";
                 }
